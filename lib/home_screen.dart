b7f738812558114d1ec077977ef370077fbce677
fulmedammes <github@amirkhadr.com>
2025-03-29 15:07:34 +0100

    Fix base-href format and update title to #9

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -42,7 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Welcome! #8')),
+      appBar: AppBar(title: const Text('Welcome! #9')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
