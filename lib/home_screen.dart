13832c4b7d9809686c7a528ec9875e17ef7593c7
fulmedammes <github@amirkhadr.com>
2025-03-29 15:26:23 +0100

    Fix paths for GitHub Pages deployment

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -42,7 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Screen! #12')),
+      appBar: AppBar(title: const Text('Home Screen! #13 - Fixed Paths')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
