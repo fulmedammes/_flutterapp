ec74f6d59645707db1b06014603f034014240ccc
fulmedammes <fulmedammes@users.noreply.github.com>
2025-04-06 04:14:04 +0000

    deploy: 507aa2144ae8523e58e2f7e43bc0931c120ba51e

---
+++ environment.js
@@ -0,0 +1,3 @@
+window.ENV = {};
+window.ENV.SUPABASE_URL = 'https://vqeabgbvrlqotehnphrk.supabase.co';
+window.ENV.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxZWFiZ2J2cmxxb3RlaG5waHJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4MzY4OTUsImV4cCI6MjA1OTQxMjg5NX0.xR6DeMnm-L4mz2GWXkiG34dWcdDphXlgUOF53aQS2f8';
